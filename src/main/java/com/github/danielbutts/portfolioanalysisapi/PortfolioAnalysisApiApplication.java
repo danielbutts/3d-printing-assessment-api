9893b9f8a5bdeee9e1edff672c1e8b0d2f473c1f
danielbutts <daniel.butts@gmail.com>
2017-07-01 23:10:22 -0700

    initial commit

---
+++ src/main/java/com/github/danielbutts/portfolioanalysisapi/PortfolioAnalysisApiApplication.java
@@ -0,0 +1,12 @@
+package com.github.danielbutts.portfolioanalysisapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioAnalysisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioAnalysisApiApplication.class, args);
+	}
+}
